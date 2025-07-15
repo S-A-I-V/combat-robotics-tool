c79d57a5d03c5dea89fab47a0623f7c6b8561094
S-A-I-V <saideep.verma01@gmail.com>
2025-07-15 23:44:30 +0530

    fix: Update header title from 'Combat Robotics Hub' to 'Bot Help'

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="bg-gradient-to-r from-blue-600 to-purple-700 text-white p-4 shadow-lg flex items-center justify-between rounded-b-lg header-fiery-bg">
       <div className="flex items-center">
-        <h1 className="text-3xl font-bold tracking-wide">Combat Robotics Hub</h1>
+        <h1 className="text-3xl font-bold tracking-wide">Bot Help</h1>
         <svg className="w-8 h-8 ml-3 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
         </svg>
